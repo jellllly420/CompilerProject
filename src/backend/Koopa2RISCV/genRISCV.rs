6b82d6589ef83c0528b539f9aaba6281a8071cef
jellllly420 <z2629365594@gmail.com>
2023-01-04 06:58:49 +0800

    lv6

--- src/backend/Koopa2RISCV/genRISCV.rs
+++ src/backend/Koopa2RISCV/genRISCV.rs
@@ -43,7 +43,7 @@ impl GenerateRISCV for FunctionData {
                 byte = byte + 4;
             }
         }
-        byte = byte + 4;
+        //byte = byte + 4;
         if byte % 16 != 0 {
             byte /= 16;
             byte *= 16;
